456971927a2fc5e4fbaeac8a34648c75f9cd0f28
guanwenhao <407818306@qq.com>
2022-07-29 09:06:51 +0800

    新增【红宝书】

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,41 +21,6 @@ module.exports = {
             { title: "接口和对象类型", path: "/study/TS/接口和对象类型" },
             { title: "数组类型", path: "/study/TS/数组类型" },
           ],
-
-    title: '前端乐园',
-    description: '',
-    head: [],
-    plugins: [],
-    themeConfig: {
-        nav: [
-            { text: '主页', link: '/' },
-            { text: '指南', link: '/guide/' },
-            { text: '生活', link: '/life/'},
-            { text: '学习',
-                items: [
-                    { text: '数学', link: '/study/math/math1' },
-                ]
-            }
-			],
-        sidebar: {
-            '/study/': [
-                {
-                    title: 'TS',
-                    collapsable: true,
-                    children: [
-                        { title: '基础类型', path: '/study/TS/基础类型' },
-                        { title: '任意类型', path: '/study/TS/任意类型' },
-                        { title: '接口和对象类型', path: '/study/TS/接口和对象类型' },
-                    ]
-                },
-                {
-                    title: '其他',
-                    collapsable: true,
-                    children: [
-                        { title: '阿松大', path: '/study/math/math1' },
-                    ]
-                }
-            ],
         },
         {
           title: "红宝书",
@@ -78,6 +43,7 @@ module.exports = {
       ],
     },
     sidebarDepth: 0, //左侧导航显示的层级
+    lastUpdated: "最后更新时间：",
     lastUpdated: "Last Updated",
   },
 };
