6097bbe03c57c11e74a6948cd841700e837701ca
guanwenhao <407818306@qq.com>
2022-08-01 18:09:14 +0800

    0801

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    title: '前端乐园',
-    description: '',
-    head: [],
-    plugins: [],
-    themeConfig: {
-        nav: [
-            { text: '主页', link: '/' },
-            { text: '指南', link: '/guide/' },
-            { text: '学习',
-                items: [
-                    { text: '数学', link: '/study/math/math1' },
-                ]
-            }
-			],
-        sidebar: {
-            '/study/': [
-                {
-                    title: 'TS',
-                    collapsable: true,
-                    children: [
-                        { title: '基础类型', path: '/study/TS/基础类型' },
-                        { title: '任意类型', path: '/study/TS/任意类型' },
-                        { title: '接口和对象类型', path: '/study/TS/接口和对象类型' },
-                    ]
-                },
-                {
-                    title: '其他',
-                    collapsable: true,
-                    children: [
-                        { title: '阿松大', path: '/study/math/math1' },
-                    ]
-                }
-            ],
-=======
   title: "前端乐园",
   description: "",
   head: [["script", { src: "/js/base.js" }]],
@@ -42,8 +7,6 @@ module.exports = {
     nav: [
       { text: "主页", link: "/" },
       { text: "指南", link: "/guide/" },
-      { text: "生活", link: "/life/" },
-      { text: "学习", items: [{ text: "数学", link: "/study/math/math1" }] },
     ],
     sidebar: {
       "/study/": [
@@ -57,7 +20,6 @@ module.exports = {
             { title: "数组类型", path: "/study/TS/数组类型" },
             { title: "函数扩展", path: "/study/TS/函数扩展" },
           ],
->>>>>>> acc90787e5fdc748e1abc1dc7f291f0211f0fe08
         },
         {
           title: "红宝书",
